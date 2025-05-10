fbd1943924030efedc52d8211e742756caa4866d
harry1722 <raldiraldi42@gmail.com>
2025-05-10 10:03:43 +0200

    Update next_permutation.py
    Find the next smallest number of a sequence.

--- next_permutation.py
+++ next_permutation.py
@@ -1,41 +1,53 @@
-
+# Funksioni për të identifikuar indeksin i parë nga fundi ku një element është më i vogël se elementi pas tij.
 def identify(x):
-    n=len(x)
-    i=n-1
-    while (i>0) and (x[i-1]>x[i]):
-        i=i-1
-    return i
+    n = len(x)  # Gjej gjatësinë e listës.
+    i = n - 1   # Fillon nga fundi i listës.
+    # Gjej indeksin më të vogël ku elementi para tij është më i madh se elementi aktual.
+    while (i > 0) and (x[i-1] > x[i]):
+        i -= 1
+    return i  # Kthen indeksin ku ndryshimi duhet të fillojë.
 
-def minimum(x,i):
-    n=len(x)
-    k=i
-    for j in range(i+1,n):
-        if (x[j]<x[k]) and (x[j]>x[i-1]):
-            k=j
-    return k
-def swap(a,b):
-    aux=a
-    a=b
-    b=aux
-    return a,b
+# Funksioni për të gjetur elementin më të vogël në intervalin [i+1, n) që është më i madh se x[i-1].
+def minimum(x, i):
+    n = len(x)  # Gjatësia e listës.
+    k = i       # Fillon kërkimin nga indeksi i.
+    # Iteron mbi listën për të gjetur elementin më të vogël më të madh se x[i-1].
+    for j in range(i+1, n):
+        if (x[j] < x[k]) and (x[j] > x[i-1]):
+            k = j
+    return k  # Kthen indeksin e elementit të zgjedhur.
 
-def reverse(x,left,right):
-    i=left
-    j=right
-    while i<j:
-       x[i],x[j]=x[j],x[i]  # other type of swap
-       i=i+1
-       j=j-1
-    return x    
+# Funksioni për të ndërruar dy vlera.
+def swap(a, b):
+    return b, a  # Kthen vlerat e ndërruara.
 
-x=[6,3,0,9,4,8,7,5,2,1]
-print “Digits of the initial number :",x
-i=identify(x)
-print "i=",i
-k=minimum(x,i)
-print "k=",k
-x[i-1],x[k]=swap(x[i-1],x[k])
-print “Sequence after swap:",x
-x=reverse(x,i,len(x)-1)
-print “Sequence after reverse:",x
+# Funksioni për të përmbysur listën mes indekseve të dhëna.
+def reverse(x, left, right):
+    i = left      # Indeksi i majtë.
+    j = right     # Indeksi i djathtë.
+    # Përmbys elementet mes indekseve left dhe right.
+    while i < j:
+        x[i], x[j] = x[j], x[i]
+        i += 1
+        j -= 1
+    return x  # Kthen listën e përditësuar.
 
+# Lista fillestare.
+x = [6, 3, 0, 9, 4, 8, 7, 5, 2, 1]
+print("Digits of the initial number:", x)
+
+# Gjej indeksin ku fillon ndryshimi për permutimin e ardhshëm.
+i = identify(x)
+print("i =", i)
+
+# Gjej elementin më të vogël më të madh se x[i-1].
+k = minimum(x, i)
+print("k =", k)
+
+# Ndërro vlerat mes x[i-1] dhe x[k].
+x[i-1], x[k] = swap(x[i-1], x[k])
+print("Sequence after swap:", x)
+
+# Përmbys pjesën e listës nga indeksi i në fund.
+x = reverse(x, i, len(x) - 1)
+print("Sequence after reverse:", x)
